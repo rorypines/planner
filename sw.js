6100ca9b79b9f27597ca0701e5317bdb6ceebd82
rorypines <corano.ver.ton@gmail.com>
2026-07-16 22:45:27 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,8 @@
 const CACHE_NAME = 'planner-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/manifest.json'
-];
+    'index.html',
+    'manifest.json'
+]
 
 self.addEventListener('install', event => {
     event.waitUntil(
