5f06651d8161619d550ddadb68d7cacb14fe61ba
rorypines <corano.ver.ton@gmail.com>
2026-07-16 22:48:05 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,31 @@
-const CACHE_NAME = 'planner-v1';
+const CACHE_NAME = "planner-v2";
 const urlsToCache = [
-    'index.html',
-    'manifest.json'
-]
-
-self.addEventListener('install', event => {
+    "./",
+    "./index.html",
+    "./manifest.json"
+];
+self.addEventListener("install", event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(urlsToCache))
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
     );
 });
-
-self.addEventListener('fetch', event => {
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.map(key => {
+                    if (key !== CACHE_NAME) {
+                        return caches.delete(key);
+                    }
+                })
+            )
+        )
+    );
+});
+self.addEventListener("fetch", event => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
     );
 });
