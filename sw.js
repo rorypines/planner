1738fb0cf11a725262d1c043d02c6e4f6c441ef7
rorypines <corano.ver.ton@gmail.com>
2026-07-17 15:34:00 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,17 @@
 const CACHE_NAME = "planner-v2";
+
 const urlsToCache = [
     "./",
     "./index.html",
     "./manifest.json"
 ];
+
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
     );
 });
+
 self.addEventListener("activate", event => {
     event.waitUntil(
         caches.keys().then(keys =>
@@ -22,6 +25,7 @@ self.addEventListener("activate", event => {
         )
     );
 });
+
 self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request).then(response => {
